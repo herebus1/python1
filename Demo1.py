15b07c9f4b66df74f7216a07097f9e40a528a4cb
herebus1 <33296126+herebus1@users.noreply.github.com>
2025-07-25 12:36:27 -0500

    Test files compiling

---
+++ Demo1.py
@@ -0,0 +1,25 @@
+#Comment example
+a=10
+b=20
+c=10.25
+d=11.25
+e=a+b
+f=c+d
+
+#To convert to string use str(variable)
+print("Integer sum " + str(e))
+print("Float sum " + str(f))
+print(type(e))
+print(type(f))
+
+#Lists examples
+list_a=["name1","name2","name3","name4"]
+print(list_a)
+print(len(list_a))
+
+#Get specific value of a list
+print(list_a[0])
+
+#Add a value to the list
+list_a.append("name5")
+print(list_a)
