93ab23b442c8b1b11e5df2d6d7c9406898352632
herebus1 <33296126+herebus1@users.noreply.github.com>
2025-07-25 13:30:19 -0500

    Test files compiling

---
+++ Demo3.py
@@ -0,0 +1,14 @@
+#defining classes and objects
+class DemoClass:
+    #Create a method and variables then access them using an object
+    a=10
+    b=20
+
+    def sum3(self):
+        print("Sample program")
+
+obj=DemoClass()
+
+print(obj.a)
+print(obj.b)
+obj.sum3()
